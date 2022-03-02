2b9e324cd3089130b6a760bf34d2679fb15bd351
bronifty <bronifty@gmail.com>
2022-03-02 15:32:37 -0500

    .

--- src/components/PlotlyComponent.jsx
+++ src/components/PlotlyComponent.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-export default () => (
-  <Plot
-    data={[
-      {
-        x: [1, 2, 3],
-        y: [2, 6, 3],
-        type: 'scatter',
-        mode: 'lines+markers',
-        marker: { color: 'red' },
-      },
-      { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
-    ]}
-    layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
-  />
-);
+export default ({ data }) => {
+  console.log(data);
+  let x = [];
+  let y = [];
+  data.map((d) => {
+    x.push(d._time);
+    y.push(d._value);
+  });
+  console.log({ x, y });
+  return (
+    <Plot
+      data={[
+        {
+          x,
+          y,
+          type: 'scatter',
+          mode: 'lines+markers',
+          marker: { color: 'red' },
+        },
+        // { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+      ]}
+      layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
+    />
+  );
+};
